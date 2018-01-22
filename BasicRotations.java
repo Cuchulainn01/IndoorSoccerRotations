9305683feceea70c799cd114df66c868cc1d274f
Unknown <nschobbe@protonmail.com>
2018-01-22 13:50:22 -0800

    First file for text based interface

---
+++ BasicRotations.java
@@ -0,0 +1,10 @@
+public class BasicRotations {
+   public static void main(String[] args) {
+   
+   }
+   public static ArrayList<String> initRoster() {
+      ArrayList<String> roster = new ArrayList<String>();
+      string[] temp = new string[] {"Devlin", "Ian", "Ryan", "Kaleb", "Tom", "Casen", "Adam", "Lucas"};
+      roster.addAll(Arrays.asList(temp));
+      
+      
